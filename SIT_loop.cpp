e4842d56363cbaae9806872ec66d51714e352303
dewashish-lambore-btech2024-sitpune <dewashish.lambore.btech2024@sitpune.edu.in>
2025-07-25 11:55:07 +0530

    Create SIT_loop.cpp

---
+++ SIT_loop.cpp
@@ -0,0 +1,16 @@
+include<iostream>
+using namespace std;
+
+int main(){
+    for(int i=1;i<=5;i++){
+        cout<<"SIT"<<endl;
+    }
+    return 0;
+}
+/*Sample Output: 
+SIT
+SIT
+SIT
+SIT
+SIT
+*/
